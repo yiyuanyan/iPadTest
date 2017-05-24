c8f572b57b6e8e5c270c16ad5696be25bd48dd14
何建新 <yiyuanhanly@126.com>
2017-05-24 22:24:46 +0800

    safasdfas

--- iPadTest/LogAndRegister/View/GGT_LoginView.h
+++ iPadTest/LogAndRegister/View/GGT_LoginView.h
@@ -9,5 +9,21 @@
 #import <UIKit/UIKit.h>
 
 @interface GGT_LoginView : UIView
+//手机号码边框
+@property(nonatomic, weak) UIView *phoneView;
+//手机号码icon
+@property(nonatomic, weak)UIImageView *phoneIcon;
+//手机号码分割线
+@property(nonatomic, weak)UIView *phoneLine;
+//手机号码TextField
+@property(nonatomic, weak)UITextField *phoneTextField;
 
+//密码边框
+@property(nonatomic, weak)UIView *passwordView;
+//密码icon
+@property(nonatomic, strong)UIImageView *passwordIcon;
+//密码分割线
+@property(nonatomic, strong) UIView *passwordLine;
+//密码TextField
+@property(nonatomic, strong) UITextField *passwordTextField;
 @end
--- iPadTest/LogAndRegister/View/GGT_LoginView.m
+++ iPadTest/LogAndRegister/View/GGT_LoginView.m
@@ -45,6 +45,7 @@
     phoneView.layer.borderWidth = 1;
     phoneView.layer.cornerRadius = 6;
     phoneView.layer.borderColor = UICOLOR_FROM_HEX(0xc40016).CGColor;
+    self.phoneView = phoneView;
     [self addSubview:phoneView];
     [phoneView mas_makeConstraints:^(MASConstraintMaker *make) {
         make.centerX.equalTo(self.mas_centerX);
@@ -55,6 +56,7 @@
     UIImageView *phoneIcon = [[UIImageView alloc] init];
     phoneIcon.image = UIIMAGE_FROM_NAME(@"iPone_have");
     [phoneView addSubview:phoneIcon];
+    self.phoneIcon = phoneIcon;
     [phoneIcon mas_makeConstraints:^(MASConstraintMaker *make) {
         make.centerY.equalTo(phoneView.mas_centerY);
         make.left.equalTo(phoneView.mas_left).with.offset(LineX(15));
@@ -64,6 +66,7 @@
     UIView *phoneLine = [UIView new];
     phoneLine.backgroundColor = UICOLOR_FROM_HEX(0xc40016);
     [phoneView addSubview:phoneLine];
+    self.phoneLine = phoneLine;
     [phoneLine mas_makeConstraints:^(MASConstraintMaker *make) {
         make.width.mas_equalTo(1);
         make.height.mas_equalTo(phoneView.mas_height);
@@ -76,11 +79,59 @@
     phoneTextField.placeholder = @"请输入手机号码";
     phoneTextField.backgroundColor = [UIColor whiteColor];
     [phoneView addSubview:phoneTextField];
+    self.phoneTextField = phoneTextField;
     [phoneTextField mas_makeConstraints:^(MASConstraintMaker *make) {
         make.left.equalTo(phoneLine.mas_right).with.offset(15);
         make.top.equalTo(phoneView.mas_top).with.offset(0);
         make.bottom.equalTo(phoneView.mas_bottom).with.offset(0);
         make.right.equalTo(phoneView.mas_right).with.offset(0);
+    }];
+    /* -----------------密码输入框---------------------- */
+    UIView *passwordView = [[UIView alloc] init];
+    passwordView.layer.masksToBounds = YES;
+    passwordView.layer.borderWidth = 1;
+    passwordView.layer.cornerRadius = 6;
+    passwordView.layer.borderColor = UICOLOR_FROM_HEX(0xd5d5d5).CGColor;
+    [self addSubview:passwordView];
+    self.passwordView = passwordView;
+    [passwordView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(phoneView.mas_bottom).with.offset(LineY(30));
+        make.centerX.equalTo(phoneView.mas_centerX);
+        make.height.mas_equalTo(LineH(44));
+        make.left.mas_equalTo(phoneView.mas_left);
+        make.right.mas_equalTo(phoneView.mas_right);
+    }];
+    //密码icon
+    self.passwordIcon = [[UIImageView alloc] init];
+    self.passwordIcon.image = UIIMAGE_FROM_NAME(@"Password_not");
+    [self.passwordView addSubview:self.passwordIcon];
+    [self.passwordIcon mas_makeConstraints:^(MASConstraintMaker *make) {
+        //make.left.equalTo(self.phoneIcon.mas_left);
+        make.centerY.equalTo(self.passwordView.mas_centerY);
+        make.left.equalTo(self.passwordView.mas_left).with.offset(LineX(16));
+        make.height.mas_equalTo(LineH(20));
+        make.width.mas_equalTo(LineW(13));
+    }];
+    //密码分割线
+    self.passwordLine = [[UIView alloc] init];
+    self.passwordLine.backgroundColor = UICOLOR_FROM_HEX(0xd5d5d5);
+    [self.passwordView addSubview:self.passwordLine];
+    [self.passwordLine mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self.phoneLine.mas_left).with.offset(0);
+        make.height.equalTo(self.passwordView.mas_height);
+        make.width.mas_equalTo(1);
+        make.top.equalTo(self.passwordView.mas_top);
+        make.bottom.equalTo(self.passwordView.mas_bottom);
+    }];
+    //密码TextField
+    self.passwordTextField = [[UITextField alloc] init];
+    self.passwordTextField.placeholder = @"请输入密码";
+    [self.passwordView addSubview:self.passwordTextField];
+    [self.passwordTextField mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self.phoneTextField.mas_left);
+        make.right.equalTo(self.phoneTextField.mas_right);
+        make.top.equalTo(self.passwordView.mas_top);
+        make.bottom.equalTo(self.passwordView.mas_bottom);
     }];
 }
 @end
