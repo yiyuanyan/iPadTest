73727a49080d7a4797ed58555a86b0732a6156f1
何建新 <yiyuanhanly@126.com>
2017-05-31 18:21:58 +0800

    123123123

--- iPadTest/Mine/Controller/GGT_SettingViewController.m
+++ iPadTest/Mine/Controller/GGT_SettingViewController.m
@@ -8,30 +8,26 @@
 
 #import "GGT_SettingViewController.h"
 
-@interface GGT_SettingViewController ()
-
+@interface GGT_SettingViewController ()<UITableViewDelegate, UITableViewDataSource>
+@property(nonatomic, strong) UITableView *tableView;
 @end
 
 @implementation GGT_SettingViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.view.backgroundColor = UICOLOR_FROM_HEX(ColorF2F2F2);
+    [self initTableView];
 }
-
+-(void)initTableView{
+    self.tableView = [[UITableView alloc] initWithFrame:CGRectMake(LineX(20), LineY(20), LineW(547), LineH(240)) style:UITableViewStylePlain];
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+#pragma mark - TableView
 
 @end
